a23c349c094de4de1701198835c3be1903798687
eaktamp <eaktamp@gmail.com>
2021-08-04 12:24:41 +0700

    sds

--- index.php
+++ index.php
@@ -292,27 +292,27 @@ $pmac = strpos($mycom, $findme);
                 // if everything is ok, try to upload file
             } else { // ไม่เจอerror รูปภาพ ทำการอัพโหลดภาพและบันทึกข้อมูลลงฐานข้อมูล
                 if (move_uploaded_file($_FILES["file_upload"]["tmp_name"], $target_file)) {
-                    echo "The file " . htmlspecialchars(basename($_FILES["file_upload"]["name"])) . " has been uploaded.";
-                    echo $pt_name = $_POST['company_name'];
-                    echo $cid = $_POST['id_card'];
-                    echo $address_number_moo = $_POST['addr'];
-                    echo $tmp_name = $_POST['district'];
-                    echo $amp_name = $_POST['amphoe'];
-                    echo $chw_name = $_POST['province'];
-                    echo $zipcode = $_POST['zipcode'];
-                    echo $phone   =  $_POST['phone'];
-                    echo $amount  =  $_POST['amount'];
-                    echo $transfer_monny = str_replace(",", "", $amount);
+                    htmlspecialchars(basename($_FILES["file_upload"]["name"])) . " has been uploaded.";
+                     $pt_name = $_POST['company_name'];
+                     $cid = $_POST['id_card'];
+                     $address_number_moo = $_POST['addr'];
+                     $tmp_name = $_POST['district'];
+                     $amp_name = $_POST['amphoe'];
+                     $chw_name = $_POST['province'];
+                     $zipcode = $_POST['zipcode'];
+                     $phone   =  $_POST['phone'];
+                     $amount  =  $_POST['amount'];
+                     $transfer_monny = str_replace(",", "", $amount);
 
-                    echo $object_name = $_POST['object_name'];
-                    echo $object_orther = $_POST['cc'];
-                    echo $transfer_bank = $_POST['transfer_bank'];
-                    echo $transfer_date = $_POST['startdate'];
-                    echo $transfer_time = $_POST['enddate'];
-                    echo $img_name =  $newimgname . '.' . $imageFileType;
-                    echo $type_check = 'N';
-                    echo $ipupdate  = get_client_ip();
-                    echo $insertdate = date('Y-m-d');
+                     $object_name = $_POST['object_name'];
+                     $object_orther = $_POST['cc'];
+                     $transfer_bank = $_POST['transfer_bank'];
+                     $transfer_date = $_POST['startdate'];
+                     $transfer_time = $_POST['enddate'];
+                     $img_name =  $newimgname . '.' . $imageFileType;
+                     $type_check = 'N';
+                     $ipupdate  = get_client_ip();
+                     $insertdate = date('Y-m-d');
 
                     $useragent = $_SERVER['HTTP_USER_AGENT'];
                     if (preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i', $useragent) || preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i', substr($useragent, 0, 4))) {
@@ -320,25 +320,23 @@ $pmac = strpos($mycom, $findme);
                     } else {
                         $device_send = "desktop";
                     }
-                    echo   $device_send = $device_send;
+                       $device_send = $device_send;
                     $macupdate = $mac = substr($mycom, ($pmac + 36), 17);
 
                     $country = file_get_contents('http://api.hostip.info/get_html.php?ip=');
                     $only_country = explode(" ", $country);
 
-                    echo $localtion_check = $only_country[1] . " " . substr($only_country[2], 0, 4);
+                     $localtion_check = $only_country[1] . " " . substr($only_country[2], 0, 4);
 
 
 
                     require_once('./config/db.php');
-                    echo "<br>";
-                    echo $sql = "INSERT INTO abhrcdb
+                    $sql = "INSERT INTO abhrcdb
             (  pt_name,cid,phone,object_name,object_orther,transfer_bank,transfer_date,transfer_time,
              transfer_monny ,img_name ,address_number_moo ,tmp_name,amp_name,chw_name ,zipcode ,
              type_check,flage,ipupdate , device_send ,macupdate,localtion_check)
              VALUES ('$pt_name','$cid','$phone','$object_name','$object_orther','$transfer_bank','$transfer_date','$transfer_time','$transfer_monny',
              '$img_name','$address_number_moo','$tmp_name','$amp_name','$chw_name','$zipcode','$type_check','Default','$ipupdate','$device_send','$macupdate','$localtion_check') ";
-                    echo "<br>";
                     $query = mysqli_query($conn, $sql);
                     if ($query) {
     ?>
